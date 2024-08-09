311c455f79af00e10acd967b8e5aff86129a3a16
jiminseong <iamjms4237@gachon.ac.kr>
2024-08-09 16:43:31 +0900

    커피챗 화면에 결제하기 -> 토스페이먼츠 작동 확인 내용 추가

--- src/views/coffeechat/api/TossPaymentAPI.tsx
+++ src/views/coffeechat/api/TossPaymentAPI.tsx
@@ -9,6 +9,11 @@ interface TossPaymentAPIProps {
 
 const TossPaymentAPI: React.FC<TossPaymentAPIProps> = ({onClick}) => {
 
+    // ------  SDK 초기화 ------
+    // TODO: clientKey는 개발자센터의 API 개별 연동 키 > 결제창 연동에 사용하려할 MID > 클라이언트 키로 바꾸세요.
+    // TODO: server.js 의 secretKey 또한 결제위젯 연동 키가 아닌 API 개별 연동 키의 시크릿 키로 변경해야 합니다.
+    // TODO: 구매자의 고유 아이디를 불러와서 customerKey로 설정하세요. 이메일・전화번호와 같이 유추가 가능한 값은 안전하지 않습니다.
+    // @docs https://docs.tosspayments.com/sdk/v2/js#토스페이먼츠-초기화
     const clientKey = process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY;
     const [payment, setPayment] = useState<any>(null);
     const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<string | null>(null);
@@ -39,7 +44,11 @@ const TossPaymentAPI: React.FC<TossPaymentAPIProps> = ({onClick}) => {
         fetchPayment();
     }, [clientKey]);
 
+    // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
+    // @docs https://docs.tosspayments.com/sdk/v2/js#paymentrequestpayment
     async function requestPayment() {
+    // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+    // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
         if (!payment) {
             console.error("Payment object is not loaded");
             return;
@@ -53,10 +62,10 @@ const TossPaymentAPI: React.FC<TossPaymentAPIProps> = ({onClick}) => {
                 currency: "KRW",
                 value: 100,
             },
-            orderId: generateRandomString(),
+            orderId: generateRandomString(), // 고유 주문번호
             orderName: "토스 티셔츠 외 2건",
-            successUrl: process.env.NEXT_PUBLIC_LOCAL_URL + "/payment/success",
-            failUrl: process.env.NEXT_PUBLIC_LOCAL_URL + "/payment/fail",
+            successUrl: window.location.origin + "/payment/success", // 결제 요청이 성공하면 리다이렉트되는 URL
+            failUrl: window.location.origin + "/fail", // 결제 요청이 실패하면 리다이렉트되는 URL
             customerEmail: "customer123@gmail.com",
             customerName: "김토스",
             customerMobilePhone: "01012341234",
